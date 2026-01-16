7796cef40f2b81dfb477ba094c6e84eef314e94c
esther <estdiasor@alu.edu.gva.es>
2026-01-16 13:35:21 +0100

    relacion operario_orden

---
+++ database/migrations/2026_01_16_122741_create_operario_orden_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operario_orden', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            //relaciones con operario y orden de trabajo
+            $table->foreignId('operario_id')
+                  ->constrained('operarios')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
+            $table->foreignId('orden_id')
+                  ->constrained('ordenesDeTrabajo')
+                  ->cascadeOnDelete()
+                  ->cascadeOnUpdate();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operario_orden');
+    }
+};
