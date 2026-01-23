2a74a1fc3f930721d66197b54af725f152768371
esther <estdiasor@alu.edu.gva.es>
2026-01-23 13:49:38 +0100

    seeders zona, parking, patio

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,5 +21,11 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call([
+            PatioSeeder::class,
+            ZonasSeeder::class,
+            ParkingsSeeder::class
+        ]);
     }
 }
---
+++ database/seeders/ZonasSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Zona;
+
+class ZonasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create('es_ES');
+
+        for($i = 0; $i < 12; $i++) {
+            Zona::create([
+                'tipo' => $faker->randomElement(['patio', 'descarga']),
+                'created_at' => date('Y-m-d'),
+                'updated_at' => date('Y-m-d')
+            ]);
+        }
+
+    }
+}
