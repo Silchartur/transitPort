ab1416d7124b60407574ea2ada3c929c7888c91e
silvia <silvia.pn97@gmail.com>
2026-02-10 09:14:56 +0100

    ME HE CARGADO SANCTUM Y PROTEGER BUQUE

--- routes/api.php
+++ routes/api.php
@@ -7,16 +7,6 @@ use App\Http\Controllers\UsuariosController;
 use App\Http\Controllers\ZonasController;
 use Illuminate\Support\Facades\Route;
 
-//RUTAS BUQUE
-Route::get('/obtenerBuques', [BuquesController::class, 'obtenerBuques'])->name('obtenerBuques');
-
-Route::view('/insertarBuque', 'insertarBuque')->name('insertarBuque');
-Route::post('/crearBuque', [BuquesController::class, 'crearBuque']) ->name('crearBuque');
-
-Route::get('/editarBuque/{id}', [BuquesController::class, 'buscarBuquePorId']) -> name('buscarBuquePorId');
-Route::patch('/actualizarBuque/{id}', [BuquesController::class, 'modificarBuque']) -> name('modificarBuque');
-
-Route::delete('/borrarBuque/{id}', [BuquesController::class, 'eliminarBuque']) -> name('eliminarBuque');
 
 //RUTAS CONTENEDOR
 Route::get('/obtenerContenedor', [ContenedoresController::class, 'obtenerContenedor'])->name('obtenerContenedor');
@@ -41,7 +31,22 @@ Route::patch('/actualizarZonas/{id}', [ZonasController::class, 'modicarEstadoZon
 //LOGIN y LOGOUT
 Route::post('/login/{rol}', [UsuariosController::class, 'login']);
 
+//MIDDLEWARE GESTOR, ADMINISTRATIVO Y OPERARIO
 Route::middleware('auth:gestor,administrativo,operario')->group(function () {
     Route::post('/logout', [UsuariosController::class, 'logout']);
+});
+
+//MIDDLEWARE GESTOR Y ADMINISTRATIVO
+Route::middleware('auth:gestor,administrativo')->group(function () {
+//RUTAS BUQUE
+Route::get('/obtenerBuques', [BuquesController::class, 'obtenerBuques'])->name('obtenerBuques');
+
+Route::view('/insertarBuque', 'insertarBuque')->name('insertarBuque');
+Route::post('/crearBuque', [BuquesController::class, 'crearBuque']) ->name('crearBuque');
+
+Route::get('/editarBuque/{id}', [BuquesController::class, 'buscarBuquePorId']) -> name('buscarBuquePorId');
+Route::patch('/actualizarBuque/{id}', [BuquesController::class, 'modificarBuque']) -> name('modificarBuque');
+
+Route::delete('/borrarBuque/{id}', [BuquesController::class, 'eliminarBuque']) -> name('eliminarBuque');
 });
 
